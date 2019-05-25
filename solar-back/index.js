23c0777a6db9fa888c2a97fe777d1b15abb01733
aleksficek <alex.ficek99@gmail.com>
2019-05-24 22:43:42 -0400

    Added api call functionality

--- solar-back/index.js
+++ solar-back/index.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-
+const request = require('request');
 app.use(cors());
 
+function initialize(api_url) {
+  return new Promise(function(resolve, reject) {
+    request.get(api_url, function(err, resp, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);  // replace (body) with (JSON.parse(body)) to parse JSON
+      }
+    })
+  })
+};
+
 app.get('/', (req, res) => {
-    res.send('we out here from the node server')
-});
-
-app.get('voltages/add', (req, res) => {
-
+    res.send('Welcome to the Solaris server')
 });
 
 app.get('/voltage', (req, res) => {
-    res.send('Sample voltage of 5')
+  const initializePromise = initialize('https://jsonplaceholder.typicode.com/todos/1')
+  initializePromise.then(function(result) {
+    res.send(result);
+  }, function(err) {
+    console.log(err);
+    res.send(err);
+  });
+});
+
+app.get('/panel_switch', (req, res) => {
+  const initializePromise = initialize('https://jsonplaceholder.typicode.com/todos/1')
+  initializePromise.then(function(result) {
+    res.send(result);
+  }, function(err) {
+    console.log(err);
+    res.send(err);
+  });
+});
+
+app.get('/back_switch', (req, res) => {
+  const initializePromise = initialize('https://jsonplaceholder.typicode.com/todos/1')
+  initializePromise.then(function(result) {
+    res.send(result);
+  }, function(err) {
+    console.log(err);
+    res.send(err);
+  });
 });
 
 app.post('/api/world', (req, res) => {
