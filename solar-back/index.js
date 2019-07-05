24e7f018276ecb9b092e11623fa2f95361ed2910
aleksficek <alex.ficek99@gmail.com>
2019-07-04 21:23:23 -0400

    Added data passage to front end

--- solar-back/index.js
+++ solar-back/index.js
@@ -2,9 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const request = require('request');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/solar', { useNewUrlParser: true });
+
+const Schema = mongoose.Schema;
+const userDataSchema = new Schema({
+  date: {type: String, required: true},
+  time: {type: String, required: true},
+  voltages: {
+    cell1: Number,
+    cell2: Number,
+    cell3: Number,
+    cell4: Number,
+    cell5: Number,
+    shunt: Number,
+    panel: Number
+  }
+}, {collection: 'test'});
+
+const UserData = mongoose.model('UserData', userDataSchema);
+
 app.use(cors());
 
-function initialize(api_url) {
+let initialize = (api_url) => {
   return new Promise(function(resolve, reject) {
     request.get(api_url, function(err, resp, body) {
       if (err) {
@@ -16,8 +36,72 @@ function initialize(api_url) {
   })
 };
 
+let decoder = (raw_data) => {
+
+};
+
+
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Solaris server')
+});
+
+app.get('/get_data', (req, res) => {
+  UserData.find()
+    .then(function(doc) {
+      res.send(doc);
+    });
+});
+
+app.get('/date_time', (req, res) => {
+
+  var datetime = JSON.stringify(new Date()).split('T');
+  datetime[0] = datetime[0].replace("\"","");
+  datetime[1] = datetime[1].replace("\"","").replace("Z","");
+  res.send(datetime);
+});
+
+app.get('/insert_data', (req, res) => {
+
+  var datetime = JSON.stringify(new Date()).split('T');
+  datetime[0] = datetime[0].replace("\"","");
+  datetime[1] = datetime[1].replace("\"","").replace("Z","");
+
+  const item = {
+    date: datetime[0],
+    time: datetime[1],
+    voltages: {
+      cell1: 0,
+      cell2: 0,
+      cell3: 0,
+      cell4: 0,
+      cell5: 0,
+      shunt: 0,
+      panel: 0
+    }
+  };
+
+  const initializePromise = initialize('https://jsonplaceholder.typicode.com/todos/1')
+  initializePromise.then(function(result) {
+    // var decoded_array = decode(result);
+    // item.voltages.cell1 = decoded_array[0];
+    // item.voltages.cell2 = decoded_array[1];
+    // item.voltages.cell3 = decoded_array[2];
+    // item.voltages.cell4 = decoded_array[3];
+    // item.voltages.cell5 = decoded_array[4];
+    // item.voltages.shunt = decoded_array[5];
+    // item.voltages.panel = decoded_array[6];
+    console.log(item.date);
+  }, function(err) {
+    console.log(err);
+    res.send(err);
+  }).then(() => {
+    let data = new UserData(item);
+    data.save();
+  });
+
+  res.send(item);
+  // res.send("The data with a date of: " + item.date + " and " + item.time + " has been added");
 });
 
 app.get('/test_voltage', (req, res) => {
--- solar-front/src/App.js
+++ solar-front/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,13 +6,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      v_cell1: '',
-      v_cell2: '',
-      v_cell3: '',
-      v_cell4: '',
-      v_cell5: '',
-      v_shunt: '',
-      v_panel: '',
+      date : '',
+      time: '',
+      voltages: {},
+      history: [],
     };
   };
 
@@ -22,61 +18,95 @@ class App extends Component {
     fontWeight: 'bold',
   }
 
+  refreshPage() {
+    window.location.reload();
+  };
 
-  componentDidMount () {
+  // Replace with below functions for automatic refresh
+  componentDidMount() {
     this.callApi();
   };
 
+  // componentDidMount() {
+  //   this.interval = setInterval(() => this.callApi(), 5000);
+  // };
+
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // };
+
   callApi() {
-    fetch('http://localhost:4000/test_voltage')
+    fetch('http://localhost:4000/insert_data')
     .then( response => {
       if (!response.ok) { throw response } // Return the complete error response for debugging purposes
       return response.json()  //we only get here if there is no error
     })
     .then( json => {
       this.setState({ 
-        v_cell1: json.v_cell1,
-        v_cell2: json.v_cell2,
-        v_cell3: json.v_cell3,
-        v_cell4: json.v_cell4,
-        v_cell5: json.v_cell5,
-        v_shunt: json.v_cell6,
-        v_panel: json.v_cell7
+        date: json.date,
+        time: json.time,
+        voltages: json.voltages,
        }); 
     })
     .catch( error => {
       this.setState({ hasErrored: true, error }) // Save both error flag and error detail so you can send it to tools like bugsnag
     })
+    .then(fetch('http://localhost:4000/get_data')
+    .then( response => {
+      if (!response.ok) { throw response } // Return the complete error response for debugging purposes
+      return response.json()  //we only get here if there is no error
+    })
+    .then( json => {
+      this.setState({ 
+        history: json
+       }); 
+       console.log(this.state.history);
+    })
+    .catch( error => {
+      this.setState({ hasErrored: true, error }) // Save both error flag and error detail so you can send it to tools like bugsnag
+    }))
   };
 
   render() {
     return (
       <div className="App">
         <div>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Date: </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.date} </span>
+        </div>
+        <div>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Time: </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.time} </span>
+        </div>
+        <div>
         <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Cell 1: </span>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.v_cell1} </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.cell1} </span>
         </div>
         <div>
         <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Cell 2: </span>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.v_cell2} </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.cell2} </span>
         </div>
         <div>
         <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Cell 3: </span>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.v_cell3} </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.cell3} </span>
         </div>
         <div>
         <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Cell 4: </span>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.v_cell4} </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.cell4} </span>
         </div>
         <div>
         <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Cell 5: </span>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.v_cell5} </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.cell5} </span>
         </div>
-
-        <button style={{ fontSize: 30 }} className="btn btn-outline-primary">Refresh </button>
-
-        {/* <h1>Voltage of Cell 1: {this.state.v_cell1}</h1> */}
-        
+        <div>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Panel: </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.panel} </span>
+        </div>
+        <div>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">Voltage of Shunt: </span>
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.state.voltages.shunt} </span>
+        </div>
+        <button onClick={this.refreshPage} style={{ fontSize: 30 }} className="btn btn-outline-primary">Refresh </button>        
       </div>
     );
   }
