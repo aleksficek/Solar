d3eaa82759dd314e420befcef0c2a675ba8ab275
Aleksander Ficek <37374704+aleksficek@users.noreply.github.com>
2019-07-10 15:58:14 -0400

    Added emergency stops and UART communication

--- solar-firm/BarebonesAPI.ino
+++ solar-firm/BarebonesAPI.ino
@@ -4,10 +4,13 @@
 ESP8266WebServer server(80);
  
 void setup() {
- 
-  Serial.begin(115200);
+
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  
   // WiFi.begin("dlink-5EA0", "bxyaw96438");  //Connect to the WiFi network
-  WiFi.begin("iPhone", "thisworks");  //Connect to the WiFi network
+  WiFi.begin("Tri-City Wifi 509", "12345678");  //Connect to the WiFi network
+  //  WiFi.begin("iPhone", "thisworks");  //Connect to the WiFi network
  
   while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
  
@@ -24,6 +27,43 @@ void setup() {
     server.send(200, "text / plain", "Other URL");
  
   });
+
+  server.on("/test_serial", []() {   //Define the handling function for the path
+
+    String sample_value = "Nothing";
+    while (Serial.available()) {
+      sample_value = Serial.read();
+    }
+    server.send(200, "text / plain", sample_value);
+ 
+  });
+
+  server.on("/test_uart", []() {   //Define the handling function for the path
+
+    String sample_value = "Nothing";
+    while (Serial.available()) {
+      char w = Serial.read();
+    }
+
+    while (Serial.peek() != 60) {
+      char e = Serial.read(); 
+    }
+    char r = Serial.read();
+  
+    sample_value = Serial.readStringUntil('>');
+    
+    server.send(200, "text / plain", sample_value);
+  });
+
+   server.on("/panel_stop", []() {   //Define the handling function for the path
+    Serial.print('p');
+    server.send(200, "text / plain", "Panel has been stopped!");
+  });
+
+   server.on("/battery_stop", []() {   //Define the handling function for the path
+    Serial.print('b');
+    server.send(200, "text / plain", "Battery has been stopped!");
+  });
  
   server.on("/", handleRootPath);    //Associate the handler function to the path
   server.begin();                    //Start the server
@@ -32,7 +72,7 @@ void setup() {
 }
  
 void loop() {
- 
+
   server.handleClient();         //Handling of incoming requests
  
 }
